fbb52c65b51ab05cfbfdecd36887899184fbcb05
Samir Prakash <Samir.Prakash@gmail.com>
2019-10-17 17:50:56 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
